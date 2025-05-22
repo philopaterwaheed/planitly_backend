97db2b86fdb1a63a5ed4045d18a43869c7e7bcf0
philosan <philopaterwaheed9@gmail.com>
2025-05-22 20:03:27 +0000

    test

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import logging
 from mongoengine import connect, disconnect
 from consts import firebase_urls
 
+test 
 # Set up logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
